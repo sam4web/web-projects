83a6c2470693a7d1eb9b66e675bc346eca83bbfb
sam4web <sijal.m06@gmail.com>
2022-08-10 22:24:20 +0545

    dynamic projects - data from json api

--- js/script.js
+++ js/script.js
@@ -2,88 +2,59 @@ $(document).ready(() => {
   $('#preloader').fadeOut();
 });
 
-const projects = [
-  {
-    id: 1,
-    name: 'xolcy',
-    sourceCode: 'https://bitbucket.org/samdoescode/xolcy/src/',
-    livePreview: 'https://xolcy-template.netlify.app/',
-  },
-  {
-    id: 2,
-    name: 'socialated',
-    sourceCode: 'https://bitbucket.org/samdoescode/socialated/src/',
-    livePreview: 'https://socialated-template.netlify.app/',
-  },
-  {
-    id: 3,
-    name: 'monst',
-    sourceCode: 'https://bitbucket.org/samdoescode/monst/src/',
-    livePreview: 'https://monst-template.netlify.app/',
-  },
-  {
-    id: 4,
-    name: 'luaz',
-    sourceCode: 'https://bitbucket.org/samdoescode/luaz/src/',
-    livePreview: 'https://luaz-template.netlify.app/',
-  },
-  {
-    id: 5,
-    name: 'hously',
-    sourceCode: 'https://bitbucket.org/samdoescode/hously/src/',
-    livePreview: 'https://hously-template.netlify.app/',
-  },
-  {
-    id: 6,
-    name: 'state info',
-    sourceCode: 'https://github.com/sam4web/state_info',
-  },
-];
+const Request = 'https://sam4web.github.io/info_data/data.json';
 
-let projectTemplate = '';
+fetch(Request)
+  .then((res) => res.json())
+  .then((data) => {
+    // get projects data form "data.json"
+    console.log(data.projects);
+    loadProjects(data.projects);
+  });
 
-// console.log($('.project-container').html());
-
-projects.forEach((project) => {
-  projectTemplate += `\n 
-  <div class="project-item ${project.id % 2 === 0 ? 'right' : 'left'}">
-    <div class="project-item__image">
-      <img src="./img/project-image/project-${
-        project.id >= 10 ? project.id : `0${project.id}`
-      }.png" alt="" />
-      <ul>
-        <li title="source code">
-          <a
-            target="_blank"
-            href="${project.sourceCode}"
-          >
-            <i class="fa-solid fa-code"></i>
-          </a>
-        </li>
-        ${
-          'livePreview' in project
-            ? `        
-        <li title="live site">
-          <a target="_blank" href="${project.livePreview}">
+// generate and load projectTemplate in class ".project-container"
+const loadProjects = (projects) => {
+  let projectTemplate = '';
+  projects.forEach((project) => {
+    console.log(project);
+    projectTemplate += `\n
+      <div class="project-item ${project.id % 2 === 0 ? 'right' : 'left'}">
+        <div class='project-item__image'>
+          <img src='${project.imageLink}' alt='${project.name}' />
+          <ul>
+            <li title='source code'>
+              <a target='_blank' href='${project.sourceCode}'>
+                <i class='fa-solid fa-code'></i>
+              </a>
+            </li>
+            ${
+              'livePreview' in project
+                ? `
+            <li title="live site">
+            <a target="_blank" href="${project.livePreview}">
             <i class="fa-solid fa-eye"></i>
+            </a>
+            </li>`
+                : ''
+            }
+          </ul>
+        </div>
+        <h3 class='project-item__title'>
+          <a
+            href="${
+              'livePreview' in project
+                ? project.livePreview
+                : project.sourceCode
+            }"
+            target='_blank'
+          >
+            ${project.name}
           </a>
-        </li>`
-            : ''
-        }
-      </ul>
-    </div>
-    <h3 class="project-item__title">
-      <a href="${
-        'livePreview' in project ? project.livePreview : project.sourceCode
-      }"
-       target="_blank">
-        ${project.name}
-      </a>
-    </h3>
-  </div>
-<div class="clear"></div>
-<!-- / project item -->
-\n`;
-});
-
-$('.project-container').html(projectTemplate);
+        </h3>
+      </div>
+      <div class='clear'></div>
+      <!-- / project item -->
+        \n`;
+  });
+  $('.project-container').html(projectTemplate);
+};
